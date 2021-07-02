d264e95ef348e3eff6790f9d5ee2d47cad231483
Romulorod <biorrodrigues@gmail.com>
2021-07-02 20:39:48 -0300

    editing and updating 100% functional

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -1,53 +1,55 @@
 <template>
-<main>
-  <link
-    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-    rel="stylesheet"
-    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-    crossorigin="anonymous"/>
-  <div>
-    <h1>Vue 3 ToDo App</h1>
-    <form @submit.prevent="addNewTodo(index)">
-      <div class="mb-3 centralized">
-        <label for="formGroupExampleInput" class="form-label"> ToDo List </label>
+  <main>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+      crossorigin="anonymous"
+    />
+    <div>
+      <h1>Vue 3 ToDo App</h1>
+      <form @submit.prevent="addNewTodo(index)">
+        <div class="mb-3 centralized">
+          <label for="formGroupExampleInput" class="form-label"> ToDo List </label>
 
-        <input
-          type="text"
-          class="form-control input"
-          id="formGroupExampleInput"
-          placeholder="New ToDo"
-          v-model="newTodo"
-        />
-      </div>
-    </form>
+          <input
+            type="text"
+            class="form-control input"
+            id="formGroupExampleInput"
+            placeholder="New ToDo"
+            v-model="newTodo"
+          />
+        </div>
+      </form>
 
-    <section class="btn-section">
-      <span v-if="!edit">
-        <button class="btn btn-success" @click.prevent="addNewTodo" type="button">Add New ToDo</button>
-      </span>
-      <span v-else>
-        <button class="btn btn-success" @click.prevent="updateTodo(index)" type="button">Update ToDo</button>
-      </span>
+      <section class="btn-section">
+        <span v-if="!edit">
+          <button class="btn btn-success" @click.prevent="addNewTodo" type="button">Add New ToDo</button>
+        </span>
+        <span v-else>
+          <button class="btn btn-success" @click.prevent="updateTodo" type="button">Update ToDo</button>
+        </span>
 
-      <button class="btn btn-warning" @click.prevent="clearInput" type="button">Clear Input Field</button>
-    </section>
-    <section class="btn-section">
-      <button @click="removeAll" class="btn btn-danger">Remove ToDos</button>
+        <button class="btn btn-warning" @click.prevent="clearInput" type="button">Clear Input Field</button>
+      </section>
+      <section class="btn-section">
+        <button @click="removeAll" class="btn btn-danger">Remove ToDos</button>
 
-      <button @click="markAllasDone" class="btn btn-warning">Mark all as Done</button>
-    </section>
+        <button @click="markAllasDone" class="btn btn-warning">Mark all as Done</button>
+      </section>
 
-    <ul>
-      <li v-for="(todo, index) in todos" :key="todo.id" class="todo">
-        <h3 :class="{ done: todo.done }" @click="toggleDone(todo)">
-          {{ todo.content }}
-        </h3>
-
-        <button @click="removeTodo(index)">Remove ToDo</button>
-        <button @click="editTodo(index)">Edit ToDo</button>
-      </li>
-    </ul>
-  </div>
+      <ul>
+        <li v-for="(todo, index) in todos" :key="todo.id" class="todo">
+          <h3 :class="{ done: todo.done }" @click="toggleDone(todo)">
+            {{ todo.content }}
+          </h3>
+          <section class="btn-section">
+          <button class="btn btn-danger" @click="removeTodo(index)">Remove ToDo</button>
+          <button class="btn btn-info" @click="editTodo(index)">Edit ToDo</button>
+          </section>
+        </li>
+      </ul>
+    </div>
   </main>
 </template>
 
@@ -59,6 +61,8 @@ export default {
     const newTodo = ref("");
     const todos = ref([]);
     let edit = ref(false);
+    let selectedIndex = ref(0);
+    let newName = ref("");
 
     function addNewTodo() {
       if (newTodo.value !== "") {
@@ -67,7 +71,6 @@ export default {
           done: false,
           content: newTodo.value
         });
-        console.log(edit.value);
       }
       newTodo.value = "";
     }
@@ -79,10 +82,13 @@ export default {
     }
     function editTodo(index) {
       edit.value = true;
-      console.log(edit.value);
       newTodo.value = todos.value[index].content;
+      selectedIndex.value = index;
     }
-    function updateTodo() {}
+    function updateTodo() {
+      todos.value.splice(selectedIndex.value, 1, {content: newTodo.value});
+      clearInput();
+    }
     function markAllasDone() {
       todos.value.forEach((todo) => (todo.done = true));
     }
@@ -90,9 +96,12 @@ export default {
       todos.value = [];
     }
     function clearInput() {
+      edit.value = false;
       newTodo.value = "";
     }
     return {
+      newName,
+      selectedIndex,
       edit,
       clearInput,
       removeAll,
@@ -143,7 +152,13 @@ ul {
   margin-inline: auto;
   margin-bottom: 2%;
 }
-
+.btn-section2 {
+  display: flex;
+  margin: 0 2% 0 2%;
+  justify-content: space-between;
+  margin-inline: auto;
+  margin-bottom: 2%;
+}
 .todo {
   cursor: pointer;
 }
